11266c9857e88f87c48cd420b5484676b311115d
RuslanBeresnev <rusber03102003@gmail.com>
2023-03-04 02:22:19 +0300

    Initial commit

---
+++ Задания с 17.02.2023/Task3/Task3/ListReversal.fs
@@ -0,0 +1,15 @@
+﻿open System
+
+let reverseList list =
+    let rec reversing source reversed =
+        if List.isEmpty source then reversed
+        else reversing (List.tail source) ((List.head source) :: reversed)
+    reversing list []
+
+printfn "Enter list length:"
+let length = Console.ReadLine() |> int
+     
+printfn "Enter elements of the list:"
+let originList = List.init length (fun _ -> Console.ReadLine() |> int)
+
+printfn "Reversed list: %A" (reverseList originList)
