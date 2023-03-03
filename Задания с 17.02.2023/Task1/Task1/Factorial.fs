a8f295d49e8608cb40636a827ed8913a7805e73e
RuslanBeresnev <rusber03102003@gmail.com>
2023-03-04 00:49:49 +0300

    Initial commit

---
+++ Задания с 17.02.2023/Task1/Task1/Factorial.fs
@@ -0,0 +1,16 @@
+﻿open System
+
+printfn "Enter number to calculate factorial:"
+let enteredNumber = Console.ReadLine() |> int
+
+let factorial number =
+    if number < 0 then -1
+    else
+        let rec tailRecursive i accumulator =
+            if i <= 1 then accumulator
+            else tailRecursive (i - 1) (i * accumulator)
+        tailRecursive number 1
+
+let result = factorial enteredNumber
+if result = -1 then printfn "Factorial doesn't exist for negative numbers"
+else printfn "Factorial of %d is %d" enteredNumber (factorial enteredNumber)
