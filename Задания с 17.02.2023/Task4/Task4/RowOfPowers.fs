2ab9dd3b1406cef629620f9b859e33e8a315b830
RuslanBeresnev <rusber03102003@gmail.com>
2023-03-04 03:21:55 +0300

    Initial commit

---
+++ Задания с 17.02.2023/Task4/Task4/RowOfPowers.fs
@@ -0,0 +1,14 @@
+﻿open System
+
+let generatePowerRow n m =
+    let rec generating list currentNumber =
+        if List.length list = m + 1 then list
+        else generating (currentNumber :: list) (currentNumber / 2)
+    generating [] ((2.0 ** float (n + m)) |> int)
+
+printfn "Enter n:"
+let n = Console.ReadLine() |> int
+printfn "Enter m:"
+let m = Console.ReadLine() |> int
+
+printfn "Generated row of powers of 2 between powers %d and %d: %A" (n) (m + n) (generatePowerRow n m)
