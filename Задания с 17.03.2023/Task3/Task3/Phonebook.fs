9030de0ab6e962c970cb2a2e9b55311c3670e536
RazbeR <rusber03102003@gmail.com>
2023-03-31 23:54:50 +0300

    Недоработанная задача

---
+++ Задания с 17.03.2023/Task3/Task3/Phonebook.fs
@@ -0,0 +1,11 @@
+﻿module Phonebook
+
+let printInfo =
+    printfn "Commands by numbers:"
+    printfn "Exit: 1"
+    printfn "Add new entry (phone and name): 2"
+    printfn "Get name by phone: 3"
+    printfn "Get phone by name: 4"
+    printfn "Print all entryes: 5"
+    printfn "Save data to file: 6"
+    printfn "Read data from file: 7"
