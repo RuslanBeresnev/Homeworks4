b0fd23e24a70c0e3666a8ee6aaf7eb75ad7a52ae
RazbeR <rusber03102003@gmail.com>
2023-04-21 12:05:34 +0300

    Task1 added

---
+++ Control Work/Task1/Task1/Supermap.fs
@@ -0,0 +1,8 @@
+﻿module Supermap
+
+let supermap mapper list =
+    let rec mapping source mapped =
+        match source with
+        | [] -> mapped
+        | h :: t -> mapping t (mapped @ (h |> mapper))
+    mapping list []
