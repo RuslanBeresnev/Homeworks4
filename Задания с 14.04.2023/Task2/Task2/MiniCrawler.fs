4a8646f0aace5c9dcdabe099c316ac585bcce136
RazbeR <rusber03102003@gmail.com>
2023-04-28 23:48:19 +0300

    Incomplete task solution added

---
+++ Задания с 14.04.2023/Task2/Task2/MiniCrawler.fs
@@ -0,0 +1,8 @@
+﻿module MiniCrawler
+
+open System.Text.RegularExpressions
+
+/// Finds all links matching with linkRegex
+let getAllLinks (html : string) =
+    [for matches in (Regex("<a href\s*=\s*\"?(https?://[^\"]+)\"?\s*>", RegexOptions.Compiled).Matches(html) : MatchCollection) 
+    -> matches.Groups.[1].Value]
